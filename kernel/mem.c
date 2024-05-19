07a83ded9a837fd5af377da4d8e293689cde31cd
Hugo Simon <hugo.simon@etu.inp-n7.fr>
2024-05-19 16:10:19 +0200

    chore: patch vmm

---
+++ kernel/mem.c
@@ -0,0 +1,50 @@
+#include <n7OS/mem.h>
+
+/**
+ * @brief Marque la page allouée
+ * 
+ * Lorsque la page a été choisie, cette fonction permet de la marquer allouée
+ * 
+ * @param addr Adresse de la page à allouer
+ */
+void setPage(uint32_t addr) {
+
+}
+
+/**
+ * @brief Désalloue la page
+ * 
+ * Libère la page allouée.
+ * 
+ * @param addr Adresse de la page à libérer
+ */
+void clearPage(uint32_t addr) {
+
+}
+
+/**
+ * @brief Fourni la première page libre de la mémoire physique tout en l'allouant
+ * 
+ * @return uint32_t Adresse de la page sélectionnée
+ */
+uint32_t findfreePage() {
+    uint32_t adresse= 0x0;
+
+    return adresse;
+}
+
+/**
+ * @brief Initialise le gestionnaire de mémoire physique
+ * 
+ */
+void init_mem() {
+
+}
+
+/**
+ * @brief Affiche l'état de la mémoire physique
+ * 
+ */
+void print_mem() {
+    
+}
